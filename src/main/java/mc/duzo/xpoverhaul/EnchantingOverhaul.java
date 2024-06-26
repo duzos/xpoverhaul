ba88add45963ed49b90349e4274529d114dfb6ec
James Hall <james.hall01@outlook.com>
2024-06-26 12:13:27 +0100

    initial commit

---
+++ src/main/java/mc/duzo/xpoverhaul/EnchantingOverhaul.java
@@ -0,0 +1,13 @@
+package mc.duzo.xpoverhaul;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EnchantingOverhaul implements ModInitializer {
+	/**
+	 * Runs the mod initializer.
+	 */
+	@Override
+	public void onInitialize() {
+
+	}
+}
